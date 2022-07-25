21389deae9316d53b01d82a5f8f386c07bfa5f3e
Manu <manu@Manus-MacBook-Pro.local>
2022-07-25 21:41:53 +0200

    adding footer translations

--- resources/lang/en/messages.php
+++ resources/lang/en/messages.php
@@ -46,5 +46,7 @@ return [
     'donate_2' => 'Change clan leader',
     'donate_3' => 'Buy tokens (5 units)',
     'donate_4' => 'Change character name',
-    "donate_notfound" => 'At this time we do not have PayPal linked to the project. But if you want, you can donate to us in exchange for the rewards already described above. If so, please contact us at <strong>info@l2euforia.com</strong> with the amount you want to contribute and your reward.'
+    "donate_notfound" => 'At this time we do not have PayPal linked to the project. But if you want, you can donate to us in exchange for the rewards already described above. If so, please contact us at <strong>info@l2euforia.com</strong> with the amount you want to contribute and your reward.',
+    "footer_copyright" => 'All rights reserved'
+
 ];
--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,9 +1,9 @@
 <div class="wrapper">
     <span style="font-size:22px"><strong>L2EUFORIA</strong></span>
     <div class="copy">
-            Lineage2 Euforia. version 1.0.0
+            Lineage2 Euforia - v 1.0.2
     </div>
     <div class="rights">
-        © Copyright. Todos los derechos reservados 2022. Make with love.
+        ©Copyright. {!! trans('messages.footer_copyright') !!} 2022. Make with love
     </div>
 </div>
