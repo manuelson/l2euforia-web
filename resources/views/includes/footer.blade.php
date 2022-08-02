30c1f2cbcd7876ffb97cc885908738f83d0bad1b
Manu <manuelsoon@gmail.com>
2022-08-02 18:13:42 +0200

    footer

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -8,5 +8,5 @@
         <br/>Contact: <a href="mailto:info@l2euforia.com">info@l2euforia.com</a>
     </div>
     <a href="https://l2topzone.com/vote/id/18367"  target="_blank" title="l2topzone" ><img src="https://l2topzone.com/vb/l2topzone-Lineage2-vote-banner-bottom-left-1.gif" style="position: fixed; z-index:99999; bottom: 0; left: 0;" alt="l2topzone.com" border="0"></a>
-
+    <a href="https://l2.topgameserver.net/lineage/voting/402/L2 euforia" target="_blank" id="edG92q90cLMw0264b6341"><div class="img-vote-server" style="border-radius: 5px; padding: 3px;width:140px;height: 140px;position: fixed;bottom: 0;right: 0;z-index: 99999;"><img src="https://i.imgur.com/yt3pR6M.gif" alt="Lineage 2 Servers -l2.topgameserver.net" title="l2.topgameserver.net - lineage 2" width="140" height="140"></div></a>
 </div>
