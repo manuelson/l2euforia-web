3f3cf094f7e48cb2bd3a7433175f37c54d8e2205
Manu <manuelsoon@gmail.com>
2022-08-02 18:17:38 +0200

    Footer vote

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -9,4 +9,5 @@
     </div>
     <a href="https://l2topzone.com/vote/id/18367"  target="_blank" title="l2topzone" ><img src="https://l2topzone.com/vb/l2topzone-Lineage2-vote-banner-bottom-left-1.gif" style="position: fixed; z-index:99999; bottom: 0; left: 0;" alt="l2topzone.com" border="0"></a>
     <a href="https://l2.topgameserver.net/lineage/voting/402/L2 euforia" target="_blank" id="edG92q90cLMw0264b6341"><div class="img-vote-server" style="border-radius: 5px; padding: 3px;width:140px;height: 140px;position: fixed;bottom: 0;right: 0;z-index: 99999;"><img src="https://i.imgur.com/yt3pR6M.gif" alt="Lineage 2 Servers -l2.topgameserver.net" title="l2.topgameserver.net - lineage 2" width="140" height="140"></div></a>
+    <a href='https://www.top100arena.com/category/lineage2?vote=99374' title='L2 Euforia'><img src='https://www.top100arena.com/hit/99374/medium' alt='Lineage 2 private server' width='88px' height='43px'></a>
 </div>
