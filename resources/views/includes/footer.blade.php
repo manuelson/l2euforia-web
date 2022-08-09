0e4e8e9d7a077ade93df43897856afc5e9548c64
Manu Gonzalez <manuel.gonzalez@interactiv4.com>
2022-08-09 09:15:33 +0200

    hopzone

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -10,5 +10,5 @@
     <a href="https://l2topzone.com/vote/id/18367"  target="_blank" title="l2topzone" ><img src="https://l2topzone.com/vb/l2topzone-Lineage2-vote-banner-bottom-left-1.gif" style="position: fixed; z-index:99999; bottom: 0; left: 0;" alt="l2topzone.com" border="0"></a>
     <a href="https://l2.topgameserver.net/lineage/voting/402/L2 euforia" target="_blank" id="edG92q90cLMw0264b6341"><div class="img-vote-server" style="border-radius: 5px; padding: 3px;width:140px;height: 140px;position: fixed;bottom: 0;right: 0;z-index: 99999;"><img src="https://i.imgur.com/yt3pR6M.gif" alt="Lineage 2 Servers -l2.topgameserver.net" title="l2.topgameserver.net - lineage 2" width="140" height="140"></div></a>
     <a href='https://www.top100arena.com/category/lineage2?vote=99374' title='L2 Euforia'><img src='https://www.top100arena.com/hit/99374/medium' alt='Lineage 2 private server' width='88px' height='43px'></a>
-    <a href="https://vgw.hopzone.net/site/vote/105145/1"><img src="https://l2.hopzone.net/assets/img/banners/vote_banners/simple_banner_3.png" alt="Vote our sever on HopZone.Net - top l2 servers" style="border:0; z-index:99999; width:140px; height: 120px;"></a>
+    <a href="https://vgw.hopzone.net/site/vote/105145/1"><img src="https://l2.hopzone.net/assets/img/banners/vote_banners/simple_banner_3.png" alt="Vote our sever on HopZone.Net - top l2 servers"></a>
 </div>
