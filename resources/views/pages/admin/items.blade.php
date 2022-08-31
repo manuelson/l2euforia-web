09be1ad444acc8d006c8d4b62768d46c664c3698
Manu Gonzalez <manuel.gonzalez@interactiv4.com>
2022-08-31 12:53:52 +0200

    refactor xml

--- resources/views/pages/admin/items.blade.php
+++ resources/views/pages/admin/items.blade.php
@@ -1,9 +1,9 @@
 @foreach ($items ?? '' as $item)
-    <div class="new">
+    <div class="new" style="background:grey;color:white;margin:10px 10px 10px 10px">
         <img src="" />
         <div class="title">Name: {{$item['item']['name']}}</div>
         <div class="title">Grade: {{$item['grade']}}</div>
-        <div class="title">Grade: {{$item['item']['qty']}}</div>
+        <div class="title">Qty: {{$item['item']['qty']}}</div>
         <img src="img/icons/{{$item['icon']}}.png" />
     </div>
 @endforeach
