90a13bf843493130ef3759bdcf877157248c35b9
Manu Gonzalez <manuel.gonzalez@interactiv4.com>
2022-08-31 12:56:21 +0200

    refactor xml

--- resources/views/pages/admin/chars.blade.php
+++ resources/views/pages/admin/chars.blade.php
@@ -1,12 +1,20 @@
-@foreach ($chars['message'] ?? '' as $item)
-    <a href="admin_items?page=1&owner_id={{$item['charId']}}&type=INVENTORY">Show inventory</a>
-    <a href="admin_items?page=1&owner_id={{$item['charId']}}&type=WAREHOUSE">Show Warehouse</a>
-    <a href="admin_items?page=1&owner_id={{$item['charId']}}&type=PAPERDOLL">Show Equiped</a>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
 
-    <div class="new" style="background:grey;padding:10px 10px 10px 10px;color:white; margin-bottom:5px">
-        <img src="" />
-        <div class="title">Name: {{$item['char_name']}}</div>
-        <div class="title">Lvl: {{$item['level']}}</div>
+@foreach ($chars['message'] ?? '' as $item)
+
+
+    <div class="card" style="width: 18rem;">
+        <div class="card-body">
+            <h5 class="card-title">{{$item['char_name']}}}</h5>
+            <h6 class="card-subtitle mb-2 text-muted">Lvl.{{$item['level']}}</h6>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="admin_items?page=1&owner_id={{$item['charId']}}&type=INVENTORY" class="card-link">Show inventory</a>
+            <a href="admin_items?page=1&owner_id={{$item['charId']}}&type=PAPERDOLL" class="card-link">Show Equipped</a>
+            <a href="admin_items?page=1&owner_id={{$item['charId']}}&type=WAREHOUSE" class="card-link">Show Warehouse</a>
+        </div>
     </div>
+
 
 @endforeach
