8b90ce62ddebc02de001410790b185d3d7cf36f6
Manu <manuelsoon@gmail.com>
2022-09-30 19:01:03 +0200

    new patch

--- resources/views/pages/download.blade.php
+++ resources/views/pages/download.blade.php
@@ -16,7 +16,7 @@
         <div class='packages'>
             <a href="https://drive.google.com/file/d/1jLTCrfsxZyECEojccV60Iya4L_s_UGwk/view?usp=sharing" class='button-alt uppercase'>{!! trans('messages.download_game') !!}</a>
             <span style="width:20px"></span>
-            <a href="https://drive.google.com/file/d/1k64tv0MxopI63fjliV6wNN9qjw5jAY-N/view?usp=sharing" class='button-alt uppercase'>{!! trans('messages.download_patch') !!}</a>
+            <a href="https://drive.google.com/file/d/1JzkzXflD-BpeRIVrkVRwLimJM00AusuY/view?usp=sharing" class='button-alt uppercase'>{!! trans('messages.download_patch') !!}</a>
         </div>
         <br><br/>
         <div style="text-align: center;font-size:50px;color:red"><strong>---> Zip password: euforia <---</strong></div>
